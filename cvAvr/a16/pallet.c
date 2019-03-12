60198a3cde8aa4387931635a91354269c92a8000
Sanglae Kim <nova0302@hotmail.com>
2019-03-12 11:06:17 +0900

    first commit

---
+++ cvAvr/a16/pallet.c
@@ -0,0 +1,17 @@
+/*
+ * pallet.c
+ *
+ * Created: 2019-03-12 ¿ÀÀü 11:01:31
+ * Author: Sanglae Kim
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
